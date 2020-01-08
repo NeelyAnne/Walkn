57307cfe20b34a2cc60dd8de197ef00f36e3ae77
Neely Anne <neely.anne@yahoo.com>
2020-01-08 11:46:52 -0500

    formatting for other components

--- client/src/components/All.js
+++ client/src/components/All.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { Component } from "react";
 import GoogleApiWrapper from "./Map";
 
-const All = props => (
-  <div>
-    <GoogleApiWrapper></GoogleApiWrapper>
-  </div>
-);
+class All extends Component {
+  render() {
+    return (
+      <div>
+        <GoogleApiWrapper></GoogleApiWrapper>
+      </div>
+    );
+  }
+}
 
 export default All;
